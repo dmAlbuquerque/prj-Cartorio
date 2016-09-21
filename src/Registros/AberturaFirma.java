a4668d5185a87da45a5d7da7613d82e63655eab8
Daniel Albuquerque <daniel@gurudosbugs.com.br>
2016-09-20 21:08:56 -0300

    Projeto

---
+++ src/Registros/AberturaFirma.java
@@ -0,0 +1,20 @@
+package Registros;
+public class AberturaFirma {
+	
+	private String rg;
+	private String cpf;
+	private String cnh;
+	/*private String ;
+	private String ;
+	private String ;
+	private String ;
+	private String ;
+	private String ;
+	private String ;*/
+	
+	public void DadosAberturaFirma(){
+		
+	}
+}
+
+
